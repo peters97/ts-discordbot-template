2556d9e34e2eaf7d7156d1bbe1f175bb5c62cb66
Daan Peters <daan@lyfter.nl>
2021-04-13 23:25:34 +0200

    Very basic bot setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Client } from "@typeit/discord";
+
+
+async function start() {
+    require('dotenv').config()
+
+    const client = new Client({
+        classes: [
+            `${__dirname}/classes/*.ts`,
+            `${__dirname}/classes/*.js`
+        ],
+        silent: false,
+        variablesChar: ":"
+    });
+
+    await client.login(process.env.DISCORD_API_TOKEN);
+}
+
+start();
